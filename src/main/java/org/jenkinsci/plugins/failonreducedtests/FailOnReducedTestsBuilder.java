c40638d92b757f3561d063f13629e8c26f3f2fdc
tbriers1 <tom.briers@telenetgroup.be>
2017-03-02 09:24:53 +0100

    removed unneeded comment

--- src/main/java/org/jenkinsci/plugins/failonreducedtests/FailOnReducedTestsBuilder.java
+++ src/main/java/org/jenkinsci/plugins/failonreducedtests/FailOnReducedTestsBuilder.java
@@ -139,7 +139,6 @@ public class FailOnReducedTestsBuilder extends Recorder implements SimpleBuildSt
         }
 
         public boolean isApplicable(Class<? extends AbstractProject> aClass) {
-            // Indicates that this builder can be used with all kinds of project types 
             return true;
         }
 
